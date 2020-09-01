faeef2421ef7466d8e207726e1b4eaaf86b0911f
Dhika Rizky <deka_r@outlook.com>
2020-09-01 22:11:38 +0800

    Moved Head tag to _app.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import App from 'next/app'
+import Head from 'next/head'
 import '../styles/main.css'
 
 class MyApp extends App {
   render (): React.ReactElement {
     const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
+    return (
+      <>
+        <Head>
+          <title>Hitung Mundur Pelunasan Buku Tahunan</title>
+          <meta name="description" content="Halaman hitung mundur batas pelunasan dana buku tahunan yang digelapkan" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
   }
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext } from 'next/document'
+import Document, { Html, Main, NextScript, DocumentInitialProps, DocumentContext } from 'next/document'
 
 class Root extends Document {
   static async getInitialProps (ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -10,10 +10,6 @@ class Root extends Document {
   render (): React.ReactElement {
     return (
       <Html>
-        <Head>
-          <title>Hitung Mundur Pelunasan Buku Tahunan</title>
-          <meta name="description" content="Halaman hitung mundur batas pelunasan dana buku tahunan yang digelapkan" />
-        </Head>
         <body>
           <Main />
           <NextScript />
